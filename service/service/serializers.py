655178ae8f9020ee34ea0adcbfd4543dabba5a10
Kozimjon <developer0829@gmail.com>
2024-04-04 20:00:35 +0500

    Price added in serializers

--- service/service/serializers.py
+++ service/service/serializers.py
@@ -1,12 +1,21 @@
 from rest_framework import serializers
 
-from service.models import Subscription
+from service.models import Subscription, Plan
+
+
+class PlanSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Plan
+        fields = '__all__'
 
 
 class SubscriptionSerializer(serializers.ModelSerializer):
     client_name = serializers.CharField(source='client.user.username')
     client_company = serializers.CharField(source='client.company')
     client_email = serializers.CharField(source='client.user.email')
+    plan = PlanSerializer()
+    service = serializers.CharField(source='service.name')
+    price = serializers.SerializerMethodField()
 
     class Meta:
         model = Subscription
@@ -14,6 +23,10 @@ class SubscriptionSerializer(serializers.ModelSerializer):
             'client_name',
             'client_company',
             'client_email',
-            'plan',
             'service',
+            'price',
+            'plan',
         )
+
+    def get_price(self, obj):
+        return obj.service.full_price - obj.service.full_price * obj.plan.discount / 100
--- service/service/views.py
+++ service/service/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Prefetch
 from rest_framework import viewsets
 
+from client.models import Client
 from service.models import Subscription
 from service.serializers import SubscriptionSerializer
 
 
 # Create your views here.
 class SubscriptionViewSet(viewsets.ReadOnlyModelViewSet):
-    queryset = Subscription.objects.all()
+    # queryset = Subscription.objects.all().prefetch_related('client').select_related('client__user')
+    queryset = Subscription.objects.all().select_related('service', 'plan').prefetch_related(
+        Prefetch('client', queryset=Client.objects.select_related('user').only('company',
+                                                                               'user__email',
+                                                                               'user__username'))
+    )
     serializer_class = SubscriptionSerializer
